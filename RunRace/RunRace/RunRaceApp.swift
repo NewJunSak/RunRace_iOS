b6bb9a6f34a66d0a38f0fa1e4f25f75bb07c93a9
bamsak <qjatjrdl963@naver.com>
2026-01-26 10:51:00 +0900

    [Create] 프로젝트 생성

---
+++ RunRace/RunRace/RunRaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunRaceApp.swift
+//  RunRace
+//
+//  Created by BOMBSGIE on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct RunRaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RunRace/RunRaceTests/RunRaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  RunRaceTests.swift
+//  RunRaceTests
+//
+//  Created by BOMBSGIE on 1/26/26.
+//
+
+import Testing
+@testable import RunRace
+
+struct RunRaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
