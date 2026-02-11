60bc2d42ac5686387d8f3fd24e133cea2a2e6ef1
bamsak <qjatjrdl963@naver.com>
2026-02-11 20:22:14 +0900

    [Feat/#12] 로컬 알림 추가 및 삭제 기능 구현

--- RunRace/RunRace/LocalNotifiactionService.swift
+++ RunRace/RunRace/LocalNotifiactionService.swift
@@ -19,8 +19,48 @@ final class LocalNotifiactionService {
     
     private func configureContent() -> UNMutableNotificationContent {
         let content = UNMutableNotificationContent()
-        content.title = "게임이 진행 중입니다."
-        content.body = "연결이 끊어질 수 있으니, 탭하여 게임으로 돌아오세요."
+        content.title = Constants.title.value
+        content.body = Constants.body.value
         return content
+    }
+}
+
+extension LocalNotifiactionService: NotificationServable {
+    func requestPermission() {
+        unNotificationService.requestAuthorization(options: [.alert, .sound]) { _, error in
+            if let error = error {
+                print("\(error)")
+            }
+        }
+    }
+    
+    func scheduleNotification() {
+        let content = configureContent()
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        let request = UNNotificationRequest(identifier: Constants.notificationId.value, content: content, trigger: trigger)
+        unNotificationService.add(request)
+    }
+    
+    func cancelNotification() {
+        unNotificationService.removePendingNotificationRequests(withIdentifiers: [Constants.notificationId.value])
+    }
+}
+
+private extension LocalNotifiactionService {
+    enum Constants {
+        case notificationId
+        case title
+        case body
+        
+        var value: String {
+            switch self {
+            case .notificationId:
+                return "disconnectWarning"
+            case .title:
+                return "게임이 진행 중입니다."
+            case .body:
+                return "연결이 끊어질 수 있으니, 탭하여 게임으로 돌아오세요."
+            }
+        }
     }
 }
