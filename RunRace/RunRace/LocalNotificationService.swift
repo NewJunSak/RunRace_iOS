f2b894b6cbf3c3fe61f7251e9898fc8aa2a8f765
bamsak <qjatjrdl963@naver.com>
2026-02-11 20:35:32 +0900

    [Feat/#12] 알림 권한에 따른 에러 처리 구현

--- RunRace/RunRace/LocalNotificationService.swift
+++ RunRace/RunRace/LocalNotificationService.swift
@@ -6,8 +6,11 @@
 //
 
 import UserNotifications
+import Combine
 
 protocol NotificationServable {
+    var errorPublisher: AnyPublisher<AppErrorProtocol, Never> { get }
+    
     func requestPermission()
     func scheduleNotification()
     func cancelNotification()
@@ -16,6 +19,7 @@ protocol NotificationServable {
 //TODO: DataTransferService에서 현재 매치가 진행중인지 Bool Subject로 처리 필요
 final class LocalNotificationService {
     private let unNotificationService = UNUserNotificationCenter.current()
+    private let errorSubject = PassthroughSubject<AppErrorProtocol, Never>()
     
     private func configureContent() -> UNMutableNotificationContent {
         let content = UNMutableNotificationContent()
@@ -26,10 +30,19 @@ final class LocalNotificationService {
 }
 
 extension LocalNotificationService: NotificationServable {
+    var errorPublisher: AnyPublisher<AppErrorProtocol, Never> {
+        errorSubject
+            .eraseToAnyPublisher()
+    }
+    
     func requestPermission() {
-        unNotificationService.requestAuthorization(options: [.alert, .sound]) { _, error in
+        unNotificationService.requestAuthorization(options: [.alert, .sound]) {[weak self] granted, error in
             if let error = error {
                 print("\(error)")
+            }
+            
+            if granted == false {
+                self?.errorSubject.send(NotificationError.isNotGranted)
             }
         }
     }
@@ -43,6 +56,28 @@ extension LocalNotificationService: NotificationServable {
     
     func cancelNotification() {
         unNotificationService.removePendingNotificationRequests(withIdentifiers: [Constants.notificationId.value])
+    }
+}
+
+// MARK: - 알림 권한에 따른 에러
+
+extension LocalNotificationService {
+    enum NotificationError: AppErrorProtocol {
+        case isNotGranted
+        
+        var message: String {
+            switch self {
+            case .isNotGranted:
+                "알림 권한이 거부되었습니다. 앱을 켜진 상태로 유지하여 게임 연결이 끊기지 않게 해주세요."
+            }
+        }
+        
+        var action: ErrorAction {
+            switch self {
+            case .isNotGranted:
+                return .alert
+            }
+        }
     }
 }
 
