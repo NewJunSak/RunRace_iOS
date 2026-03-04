ab3a737be4ed881fa929234e3e722a206120bae7
bamsak <qjatjrdl963@naver.com>
2026-03-04 17:06:26 +0900

    [Feat/#20] 현재 사용자의 Status업데이트 구현 (SwiftConcurrecy)

--- RunRace/RunRace/UpdateRunningStatusUseCase.swift
+++ RunRace/RunRace/UpdateRunningStatusUseCase.swift
@@ -6,11 +6,16 @@
 //
 
 import Foundation
+import Combine
 
 final class UpdateRunningStatusUseCase {
     private let pointService: PointServable
     private let dataTransferService: DataTransferService
     private let runningSession: RunningSession
+    
+    // 임시의 최종 달릴 거리
+    private let limitDistance = 100.0
+    private var task: Task<Void, Never>?
     
     init(
         pointService: PointServable,
@@ -20,5 +25,50 @@ final class UpdateRunningStatusUseCase {
         self.pointService = pointService
         self.dataTransferService = dataTransferService
         self.runningSession = runningSession
+    }
+}
+
+// MARK: - Swift Concurrency
+
+extension UpdateRunningStatusUseCase {
+    func updateStatusWithSwiftConcurrency() {
+        task = Task {
+            do {
+                //TODO: 현재 사용자의 ID관리
+                guard let runner = await runningSession.fetchRunner(id: "temp") else { return }
+                
+                for await point in pointService.pointPublisher.values {
+                    let status: RunStatus
+                    
+                    switch  runner.status {
+                    case .countDown:
+                        status = .started(point: point, time: Date())
+                    case .started:
+                        status = .running(point: point)
+                    case .running:
+                        // 현재 거리에 따른 finished, running 처리
+                        if runner.newDistance(point) < limitDistance {
+                            status = .running(point: point)
+                        } else {
+                            status = .finished(point: point, time: Date())
+                        }
+                    case .finished, .giveUp:
+                        task?.cancel()
+                    }
+                    runner.update(status: status)
+                    try dataTransferService.sendData(with: status)
+                }
+            } catch {
+                //TODO: Error
+            }
+        }
+    }
+}
+
+
+private extension Runner {
+    func newDistance(_ currentPoint: Point) -> Double {
+        guard let lastPoint = self.points.last else { return 0 }
+        return distance + currentPoint.distance(from: lastPoint)
     }
 }
