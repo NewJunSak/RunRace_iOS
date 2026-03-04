3f1ba5d5b279a44a1aa3ea6a3cc6592ba1341765
bamsak <qjatjrdl963@naver.com>
2026-03-04 17:54:19 +0900

    [Feat/#20] 현재 사용자의 Status업데이트 구현 (Combine)

--- RunRace/RunRace/UpdateRunningStatusUseCase.swift
+++ RunRace/RunRace/UpdateRunningStatusUseCase.swift
@@ -15,7 +15,10 @@ final class UpdateRunningStatusUseCase {
     
     // 임시의 최종 달릴 거리
     private let limitDistance = 100.0
+    // Concurrency
     private var task: Task<Void, Never>?
+    // Combine
+    private var cancellable: AnyCancellable?
     
     init(
         pointService: PointServable,
@@ -25,6 +28,11 @@ final class UpdateRunningStatusUseCase {
         self.pointService = pointService
         self.dataTransferService = dataTransferService
         self.runningSession = runningSession
+    }
+    
+    deinit {
+        task?.cancel()
+        task = nil
     }
 }
 
@@ -53,8 +61,10 @@ extension UpdateRunningStatusUseCase {
                             status = .finished(point: point, time: Date())
                         }
                     case .finished, .giveUp:
+                        pointService.stopUpdatingPoint()
                         task?.cancel()
                     }
+                    //runner가 클래스라 자체에서 업데이트 하도록 하긴 했는데, runningSession으로 접근해서 업데이트하는 것이 좋은지 고민
                     runner.update(status: status)
                     try dataTransferService.sendData(with: status)
                 }
@@ -65,6 +75,55 @@ extension UpdateRunningStatusUseCase {
     }
 }
 
+
+// MARK: - Combine
+
+extension UpdateRunningStatusUseCase {
+    func updateStatusWithCombine() {
+        cancellable = pointService.pointPublisher
+            .tryAsyncMap { [weak self] point -> RunStatus? in
+                guard let self,
+                      let runner = await self.runningSession.fetchRunner(id: "temp")
+                else {
+                    return nil
+                }
+                
+                let status: RunStatus
+                
+                switch runner.status {
+                case .countDown:
+                    status = .started(point: point, time: Date())
+                case .started:
+                    status = .running(point: point)
+                case .running:
+                    if runner.newDistance(point) < self.limitDistance {
+                        status = .running(point: point)
+                    } else {
+                        status = .finished(point: point, time: Date())
+                    }
+                case .finished, .giveUp:
+                    return runner.status
+                }
+                // sink 오퍼레이터에서 Task를 만들어 사용 시, 각각의 Task 생성으로 인해 처리 순서가 보장되지 않음
+                await runningSession.update(status: status, for: "temp")
+                try dataTransferService.sendData(with: status)
+                return status
+            }
+            .sink {[weak self] status in
+                guard let status else { return }
+                switch status {
+                case .finished, .giveUp:
+                    self?.pointService.stopUpdatingPoint()
+                    self?.cancellable?.cancel()
+                default :
+                    break
+                }
+            }
+        
+    }
+}
+
+// MARK: - Runner+
 
 private extension Runner {
     func newDistance(_ currentPoint: Point) -> Double {
